e4cb1442a51fb7041442dce417a2d2dd24edcbe0
Waya <waenara.dev@gmail.com>
2025-05-17 21:04:01 +0300

    bruh update

--- docker/install.sh
+++ docker/install.sh
@@ -9,20 +9,12 @@ echo "###############################################################"
 
 # Install dependencies
 apt-get update
-apt-get install -y curl wget unzip libicu-dev lib32gcc-s1 rsync
+apt-get install -y unzip libicu-dev lib32gcc-s1
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
 # Remove old binaries
 rm -rf /mnt/server/.bin
-
-# Download setup files
-cd /mnt/server
-curl -L https://github.com/Waenara/SCPSL-Egg/archive/refs/heads/main.zip -o repo.zip
-unzip repo.zip "SCPSL-Egg-main/docker/setup/*" -d .
-rsync -av --ignore-existing SCPSL-Egg-main/docker/setup/ ./
-rm -rf repo.zip SCPSL-Egg-main
-chmod +x .bin/PluginInstaller/start.sh .bin/start.sh
 
 # Download SteamCMD
 mkdir -p /mnt/server/.bin/SteamCMD
@@ -36,7 +28,7 @@ curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.g
 if [ "$EXILED_INSTALLATION" -ne 0 ]; then
     mkdir -p /mnt/server/.bin/ExiledInstaller
     cd /mnt/server/.bin/ExiledInstaller
-    wget https://github.com/ExMod-Team/EXILED/releases/latest/download/Exiled.Installer-Linux
+    curl -L "https://github.com/ExMod-Team/EXILED/releases/latest/download/Exiled.Installer-Linux" -o Exiled.Installer-Linux
     chmod +x Exiled.Installer-Linux
     ./Exiled.Installer-Linux --path /mnt/server/.bin/SCPSLDS --appdata /mnt/server/.config/ --exiled /mnt/server/.config/ $([ "$EXILED_INSTALLATION" -eq 2 ] && echo --pre-releases)
 fi
@@ -45,20 +37,24 @@ fi
 if [ "$SCPDISCORD_INSTALLATION" -eq 1 ]; then
     mkdir -p /mnt/server/.bin/SCPDiscord
     cd /mnt/server/.bin/SCPDiscord
-    wget https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/SCPDiscordBot_Linux
-    chmod +x SCPDiscordBot_Linux
+    curl -L "https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/SCPDiscordBot_Linux_SC" -o SCPDiscordBot_Linux_SC
+    chmod +x SCPDiscordBot_Linux_SC
     mkdir -p /mnt/server/.config/SCPDiscord
 
     mkdir -p /mnt/server/.config/'SCP Secret Laboratory'/PluginAPI/plugins/global/
     cd /mnt/server/.config/'SCP Secret Laboratory'/PluginAPI/plugins/global/
-    wget https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/dependencies.zip
+    curl -L "https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/dependencies.zip" -o dependencies.zip
     unzip -o dependencies.zip -d /mnt/server/.config/'SCP Secret Laboratory'/PluginAPI/plugins/global/
     rm -f dependencies.zip
     rm -f /mnt/server/.config/'SCP Secret Laboratory'/PluginAPI/plugins/global/SCPDiscord.dll
-    wget https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/SCPDiscord.dll
+    curl -L "https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/SCPDiscord.dll" -o SCPDiscord.dll
 else
     rm /mnt/server/.config/'SCP Secret Laboratory'/PluginAPI/plugins/global/SCPDiscord.dll
 fi
+
+# Download startup file
+curl -L "https://github.com/Waenara/SCPSL-Egg/raw/refs/heads/main/docker/start.sh" -o /mnt/server/start.sh
+chmod +x /mnt/server/start.sh
 
 # End of installation
 echo "###############################################################"
